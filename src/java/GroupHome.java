5d65d90d3b0196f861dee5687eb548c0e39594d3
luca-zamboni <lucazamboni92@gmail.com>
2013-12-02 09:01:03 +0100

    collapse files

--- src/java/GroupHome.java
+++ src/java/GroupHome.java
@@ -32,7 +32,7 @@ public class GroupHome extends HttpServlet {
     private String username;
     private HttpServletRequest mReq;
     private HttpServletResponse mResp;
-    
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
@@ -42,14 +42,14 @@ public class GroupHome extends HttpServlet {
             HttpSession session = req.getSession();
             username = (String) session.getAttribute(Login.SESSION_USER);
             groupid = Integer.parseInt(req.getParameter("g"));
-            
+
             generateHtml();
         } catch (Exception e) {
             Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, e);
         }
     }
-    
-    private void generateHtml() throws IOException, SQLException{
+
+    private void generateHtml() throws IOException, SQLException {
         PrintWriter pw = mResp.getWriter();
         String body = "";
         body += "<style>h2{text-align:center}</style>";
@@ -57,60 +57,79 @@ public class GroupHome extends HttpServlet {
         body += getStringError();
         body += generateHtmlAllFile();
         body += generateThread();
-        body += "<form method='POST' action='addPost?g="+groupid+"' enctype='multipart/form-data'>"
-                    + generateNewPostForm()
-                    + "</form>";
-        
-        pw.print(HtmlH.addHtml(body,username,dbm.getAvatar(dbm.getIdFromUser(username))));
-        
+        body += "<form method='POST' action='addPost?g=" + groupid + "' enctype='multipart/form-data'>"
+                + generateNewPostForm()
+                + "</form>";
+
+        pw.print(HtmlH.addHtml(body, username, dbm.getAvatar(dbm.getIdFromUser(username))));
+
     }
-    
-    private String generateHtmlAllFile(){
+
+    private String generateHtmlAllFile() {
         ArrayList<String> a = getAllFileGroup(mReq, groupid);
         String ret = "";
+        ret += "<div class=\"panel-group\" id=\"accordion\">\n"
+                + "  <div class=\"panel panel-default\">\n"
+                + "    <div class=\"panel-heading\">\n"
+                + "      <h4 class=\"panel-title\">\n"
+                + "        <a data-toggle=\"collapse\" data-parent=\"#accordion\" href=\"#collapseOne\">\n"
+                + "          Files\n"
+                + "        </a>\n"
+                + "      </h4>\n"
+                + "    </div>"
+                + "    <div id=\"collapseOne\" class=\"panel-collapse collapse\">\n"
+                + "      <div class=\"panel-body\">\n";
         ret += "\n<div class=\"list-group\">\n";
-        for(String aux : a){
-            ret += "<a class=\"list-group-item\"href='files/"+groupid+"/"+aux+"'>" +aux + "</a>\n" ;
-        }
-        ret += "\n</div>";
+         for(String aux : a){
+         ret += "<a class=\"list-group-item\"href='files/"+groupid+"/"+aux+"'>" +aux + "</a>\n" ;
+         }
+         ret += "\n</div>";
+                ret += "      </div>\n"
+                + "    </div>"
+                + "</div></div><br>";
+        /*ret += "\n<div class=\"list-group\">\n";
+         for(String aux : a){
+         ret += "<a class=\"list-group-item\"href='files/"+groupid+"/"+aux+"'>" +aux + "</a>\n" ;
+         }
+         ret += "\n</div>";*/
         return ret;
     }
-    
-    private String generateNewPostForm(){
+
+    private String generateNewPostForm() {
         String form = "";
-        
-        form += "<div class=\"form-group\">\n" +
-                "<label for=\"exampleInputFile\">File input</label>\n" +
-                "<input type=\"file\" name=\"files\" multiple/>\n" +
-                "<p class=\"help-block\">Insert your files</p>\n" +
-                "</div>\n";
-        form+="<textarea name=\"post\" class=\"form-control\" rows=\"6\"></textarea><br>";
-        form+=HtmlH.generateButtonSubmit("Submit", "btn btn-success btn-lg pull-right");
-        
+
+        form += "<div class=\"form-group\">\n"
+                + "<label for=\"exampleInputFile\">File input</label>\n"
+                + "<input type=\"file\" name=\"files\" multiple/>\n"
+                + "<p class=\"help-block\">Insert your files</p>\n"
+                + "</div>\n";
+        form += "<textarea name=\"post\" class=\"form-control\" rows=\"6\"></textarea><br>";
+        form += HtmlH.generateButtonSubmit("Submit", "btn btn-success btn-lg pull-right");
+
         return form;
     }
-    
-    private String generateThread() throws SQLException{
+
+    private String generateThread() throws SQLException {
         String thread = "";
         ArrayList<Post> p = dbm.getAllPost(groupid);
-        for(Post h : p){
+        for (Post h : p) {
             thread += "<div class=\"well\">\n";
-            thread += "<div style=\"font-size:16px;\">"+HtmlH.getImageAvatarSmallTumb(dbm.getOwnerPostAvatar(h.getOwner()))+" <b>" + dbm.getUserFormId(h.getOwner())
+            thread += "<div style=\"font-size:16px;\">" + HtmlH.getImageAvatarSmallTumb(dbm.getOwnerPostAvatar(h.getOwner())) + " <b>" + dbm.getUserFormId(h.getOwner())
                     + "</b> <span style=\"font-size:12px;\">says:</span>"
                     + "</div> <br>\n";
             thread += h.getText();
-            thread += "<br><br><div style=\"font-size:12px;\">Posted on "+
-                    "" + HtmlH.getDateFromTimestampLong(h.getCreationdate()) + "</div>";
+            thread += "<br><br><div style=\"font-size:12px;\">Posted on "
+                    + "" + HtmlH.getDateFromTimestampLong(h.getCreationdate()) + "</div>";
             thread += "</div>";
         }
         return thread;
     }
-    
-    public static ArrayList<String> getAllFileGroup(HttpServletRequest req,int groupid){
+
+    public static ArrayList<String> getAllFileGroup(HttpServletRequest req, int groupid) {
         ArrayList<String> ret = new ArrayList();
         String path = req.getServletContext().getRealPath("/");
-        File f = new File(path+"/files/"+groupid+"/");
-        if(f.listFiles() != null){
+        File f = new File(path + "/files/" + groupid + "/");
+        if (f.listFiles() != null) {
             for (final File fileEntry : f.listFiles()) {
                 if (!fileEntry.isDirectory()) {
                     ret.add(fileEntry.getName());
@@ -119,25 +138,25 @@ public class GroupHome extends HttpServlet {
         }
         return ret;
     }
-    
-    private String getStringError(){
+
+    private String getStringError() {
         String err = "";
         String nFile = mReq.getParameter("err");
         if (nFile != null) {
             int nf = Integer.parseInt(nFile);
-            if(nf == 1){
-                err += "<div class=\"alert alert-danger\">\n" +
-                        nFile + " file has not been saved because too big!!\n" +
-                        "</div>";
-            }else{
-                err += "<div class=\"alert alert-danger\">\n" +
-                        nFile + " files have not been saved because are too big!!\n" +
-                        "</div>";
+            if (nf == 1) {
+                err += "<div class=\"alert alert-danger\">\n"
+                        + nFile + " file has not been saved because too big!!\n"
+                        + "</div>";
+            } else {
+                err += "<div class=\"alert alert-danger\">\n"
+                        + nFile + " files have not been saved because are too big!!\n"
+                        + "</div>";
             }
         }
         return err;
     }
-    
+
     private void connectToDatabase(HttpServletRequest req) {
         try {
             dbm = new DBManager(req);
