c05604af90bf0e8dceb05db2fb7357e0d728813a
Giovanni De Francesco <posta.giovanni.defrancesco@gmail.com>
2014-01-28 22:43:48 +0100

    forgot file

---
+++ projectProgWeb2/web/js/groupcheckboxes.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function() {
+    var toggleInviti = function(obj) {
+        if ($(obj).prop('checked')) {
+            $("#inviti").slideDown();
+        }
+        else {
+            $("#inviti").slideUp();
+        }
+    };
+    $("input[type='checkbox']").on('change', function() {
+        toggleInviti($(this));
+    });
+    toggleInviti($("input[type='checkbox']"));
+});
