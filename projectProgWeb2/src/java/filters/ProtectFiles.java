67826532778a0fb3cdcd382187bdb8f376a9592c
Giulio Fornasaro <forna.991@gmail.com>
2014-02-05 20:48:53 +0100

    another filter bug

--- projectProgWeb2/src/java/filters/ProtectFiles.java
+++ projectProgWeb2/src/java/filters/ProtectFiles.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -26,7 +29,7 @@ import utils.Support;
  * @author luca
  */
 public class ProtectFiles implements Filter {
-    
+
     private FilterConfig filterConfig = null;
 
     @Override
@@ -36,30 +39,41 @@ public class ProtectFiles implements Filter {
 
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
+
+        DBManager dbm = Support.getDBManager(req);
         
+        if (dbm==null || DBManager.con==null) {
+            try {
+                dbm = new DBManager(req);
+                Support.putDBManager(req, dbm);
+            } catch (SQLException ex) {
+                Logger.getLogger(ProtectFiles.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
         StringBuffer uri = req.getRequestURL();
         String[] split = uri.toString().split("[/]");
         int i;
         for (i = 0; i < split.length; i++) {
-            if (split[i].equals("files")) break;
-        }
-        i+=2;
-        try{
-            
-            DBManager dbm = Support.getDBManager(req);
-            
-            int gr = Integer.parseInt(split[i-1]);
-            UserBean user = (UserBean) Support.getInSession(req, SessionUtils.USER);
-            if(!dbm.isPrivateGroup(gr) || user!=null && (dbm.isInGroup(user.getUserID(), gr) || dbm.isModerator(user.getUserID()))){
-                chain.doFilter(request, response);
-            }else{
-                res.sendRedirect(((HttpServletRequest) request).getContextPath()+"/home");
+            if (split[i].equals("files")) {
+                break;
             }
-            
-        }catch(Exception e){
-            res.sendRedirect(((HttpServletRequest) request).getContextPath()+"/home");
         }
-        
+        i += 2;
+        try {
+
+            int gr = Integer.parseInt(split[i - 1]);
+            UserBean user = (UserBean) Support.getInSession(req, SessionUtils.USER);
+            if (!dbm.isPrivateGroup(gr) || user != null && (dbm.isInGroup(user.getUserID(), gr) || dbm.isModerator(user.getUserID()))) {
+                chain.doFilter(request, response);
+            } else {
+                res.sendRedirect(((HttpServletRequest) request).getContextPath() + "/home");
+            }
+
+        } catch (Exception e) {
+            res.sendRedirect(((HttpServletRequest) request).getContextPath() + "/home");
+        }
+
     }
 
     @Override
@@ -71,5 +85,5 @@ public class ProtectFiles implements Filter {
     public void init(FilterConfig filterConfig) {
         this.filterConfig = filterConfig;
     }
-    
+
 }
---
+++ projectProgWeb2/src/java/filters/Redirector.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author forna
+ */
+public class Redirector implements Filter {
+    
+    FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig=filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        ((HttpServletResponse) response).sendRedirect(((HttpServletRequest) request).getContextPath()+"/index");
+    }
+
+    @Override
+    public void destroy() {
+        this.filterConfig=filterConfig;
+    }
+    
+}
